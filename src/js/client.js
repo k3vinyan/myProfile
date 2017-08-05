a843996aab0206869523221bcb1c21ae1f30420e
Kevin Yan <k3vinyan@gmail.com>
2017-08-05 01:41:23 -0700

    inital commit

---
+++ src/js/client.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component{
+  render(){
+    return(
+      <h1>It works!</h1>
+    );
+  }
+}
+
+class Navbar extends React.Component{
+  render(){
+    return(
+      <nav>
+        <ul>
+          <li><a href="#">ABOUT</a></li>
+          <li><a href="#">WORK</a></li>
+          <li><a href="#">CONTACT</a></li>
+        </ul>
+        <div className="btn">Hello</div>
+      </nav>
+    )
+  }
+}
+
+const nav = document.getElementById('nav');
+ReactDOM.render(<Navbar/>, nav);
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
